fc3dfb750b6124c57e2d1a99a88edc6bbc6cb07e
rithgroove <rithgroove@gmail.com>
2025-11-26 07:01:29 +0700

    add linear sampling

--- src/graphics/opengl_texture.cpp
+++ src/graphics/opengl_texture.cpp
@@ -4,7 +4,9 @@
 
 namespace retronomicon::opengl::graphics{
     OpenGLTexture::OpenGLTexture(std::shared_ptr<ImageAsset> image)
-        : m_textureId(0), m_width(image->getWidth()), m_height(image->getHeight()) 
+    : m_textureId(0)
+    , m_width(image->getWidth())
+    , m_height(image->getHeight())
     {
         if (m_width <= 0 || m_height <= 0) {
             throw std::runtime_error("OpenGLTexture: invalid image dimensions");
@@ -14,33 +16,36 @@ namespace retronomicon::opengl::graphics{
         glGenTextures(1, &m_textureId);
         glBindTexture(GL_TEXTURE_2D, m_textureId);
 
-        // Texture parameters (basic defaults, tweak later)
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);  
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);  
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);  
-        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);  
+        // Set wrapping (normal)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_S, GL_CLAMP_TO_EDGE);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_WRAP_T, GL_CLAMP_TO_EDGE);
 
-        // Figure out pixel format
+        // Set filtering (linear sampling)
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR);
+        glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+        // Pixel format detection
         GLint format = (image->getChannels() == 4) ? GL_RGBA : GL_RGB;
 
-        // Upload pixel data to GPU
+        // Upload texture
         glTexImage2D(
             GL_TEXTURE_2D,
-            0,                  // mipmap level
-            format,             // internal format
+            0,
+            format,          // internal format
             m_width,
             m_height,
-            0,                  // border
-            format,             // data format
-            GL_UNSIGNED_BYTE,   // data type
+            0,
+            format,          // input format
+            GL_UNSIGNED_BYTE,
             image->getPixels().data()
         );
 
-        // Optional: generate mipmaps
+        // Optional but recommended: Mipmaps
         glGenerateMipmap(GL_TEXTURE_2D);
 
         glBindTexture(GL_TEXTURE_2D, 0);
     }
+
 
     OpenGLTexture::~OpenGLTexture() {
         if (m_textureId != 0) {
