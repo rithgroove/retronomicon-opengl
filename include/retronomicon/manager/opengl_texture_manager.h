6722d33aae13aff0ba1a9e451327c2cd24d07206
rithgroove <rithgroove@gmail.com>
2025-12-05 06:10:02 +0700

    fix texture manager to handle font

--- include/retronomicon/manager/opengl_texture_manager.h
+++ include/retronomicon/manager/opengl_texture_manager.h
@@ -3,16 +3,24 @@
 #include <memory>
 #include "retronomicon/graphics/texture.h"
 #include "retronomicon/manager/texture_manager.h"
-#include "retronomicon/asset/image_asset.h"  
+#include "retronomicon/asset/image_asset.h"
+#include "retronomicon/asset/font_asset.h"
 
-namespace retronomicon::opengl::manager{
-	using retronomicon::manager::TextureManager;
+namespace retronomicon::opengl::manager {
+
+    using retronomicon::manager::TextureManager;
     using retronomicon::graphics::Texture;
     using retronomicon::asset::ImageAsset;
-	class OpenGLTextureManager:public TextureManager{
-		public:
-			OpenGLTextureManager();
-			// ~OpenGLTextureManager();
-			std::shared_ptr<Texture>  createTexture(std::shared_ptr<ImageAsset> ImageAsset) override;
-	};
-}
+    using retronomicon::asset::FontAsset;
+
+    class OpenGLTextureManager : public TextureManager {
+    public:
+        OpenGLTextureManager();
+
+        std::shared_ptr<Texture> createTexture(std::shared_ptr<ImageAsset> imageAsset) override;
+
+        // NEW OVERLOAD FOR FONT ASSET
+        std::shared_ptr<Texture> createTexture(std::shared_ptr<FontAsset> fontAsset) override;
+    };
+
+} // namespace retronomicon::opengl::manager
--- src/manager/opengl_texture_manager.cpp
+++ src/manager/opengl_texture_manager.cpp
@@ -1,14 +1,47 @@
 #include "retronomicon/manager/opengl_texture_manager.h"
 #include "retronomicon/graphics/opengl_texture.h"
-namespace retronomicon::opengl::manager{
-	using retronomicon::opengl::graphics::OpenGLTexture;
-	OpenGLTextureManager::OpenGLTextureManager(){
+#include "retronomicon/asset/opengl_font_asset.h"
 
-	}
+namespace retronomicon::opengl::manager {
 
-	// OpenGLTextureManager::~OpenGLTextureManager()= default;
+using retronomicon::opengl::graphics::OpenGLTexture;
 
-	std::shared_ptr<Texture>  OpenGLTextureManager::createTexture(std::shared_ptr<ImageAsset> imageAsset){
-		return std::make_shared<OpenGLTexture>(imageAsset);
-	}
+OpenGLTextureManager::OpenGLTextureManager() {}
+
+std::shared_ptr<Texture>
+OpenGLTextureManager::createTexture(std::shared_ptr<ImageAsset> imageAsset) {
+    return std::make_shared<OpenGLTexture>(imageAsset);
 }
+
+// ------------------------------------------------------------
+// NEW: Create a texture from a FontAsset (font atlas)
+// ------------------------------------------------------------
+std::shared_ptr<Texture>
+OpenGLTextureManager::createTexture(std::shared_ptr<FontAsset> fontAsset) 
+{
+    // Downcast to backend font object
+    auto glFont = std::dynamic_pointer_cast<retronomicon::opengl::asset::OpenGLFontAsset>(fontAsset);
+    if (!glFont) {
+        std::cerr << "[OpenGLTextureManager] ERROR: FontAsset is not OpenGLFontAsset.\n";
+        return nullptr;
+    }
+
+    const auto& pixels = glFont->getAtlasPixels();
+    int width = glFont->getAtlasWidth();
+    int height = glFont->getAtlasHeight();
+
+    if (pixels.empty() || width == 0 || height == 0) {
+        std::cerr << "[OpenGLTextureManager] ERROR: OpenGLFontAsset has no atlas.\n";
+        return nullptr;
+    }
+
+    // Uses the raw-buffer constructor we added earlier
+    return std::make_shared<OpenGLTexture>(
+        pixels.data(),
+        width,
+        height,
+        4 // RGBA atlas
+    );
+}
+
+} // namespace retronomicon::opengl::manager
